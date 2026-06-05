e6843391a0255c7fce271d9a444ab7c9cf0cbb5e
amey2612 <ameykhot2612@gmail.com>
2026-06-05 15:12:07 +0530

    fix(day17): make routes public, remove auth redirect loop

--- DAY17/Piece-1-Deploy to Azure Static Web Apps/quotes-angular/src/app/app.routes.ts
+++ DAY17/Piece-1-Deploy to Azure Static Web Apps/quotes-angular/src/app/app.routes.ts
@@ -1,25 +1,18 @@
 import { Routes } from '@angular/router';
 import { QuotesListComponent } from './quotes-list/quotes-list.component';
-import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'quotes', pathMatch: 'full' },
-  {
-    path: 'quotes',
-    component: QuotesListComponent,
-    canActivate: [authGuard],
-  },
+  { path: 'quotes', component: QuotesListComponent },
   {
     path: 'quotes/:id',
     loadComponent: () =>
       import('./quote-detail/quote-detail.component').then(m => m.QuoteDetailComponent),
-    canActivate: [authGuard],
   },
   {
     path: 'create-quote',
     loadComponent: () =>
       import('./create-quote-page/create-quote-page.component').then(m => m.CreateQuotePageComponent),
-    canActivate: [authGuard],
   },
   {
     path: 'login',
